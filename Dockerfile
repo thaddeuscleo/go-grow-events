31814817dedaf986d7cee1c0cf57e2a64bb7267e
Thaddeus Cleo <thaddeuscleo@gmail.com>
2023-08-03 11:18:56 +0000

    fix: remove env

--- Dockerfile
+++ Dockerfile
@@ -11,6 +11,12 @@ RUN go build -o /opt/go-grow/app cmd/app/main.go
 # Deploy Image
 FROM alpine:3.18
 
+ENV DB_USER none
+ENV DB_PASSWORD none
+ENV DB_HOST none
+ENV DB_NAME none
+ENV DB_CHARSET none
+
 WORKDIR /opt/go-grow
 COPY --from=Build /opt/go-grow/app /opt/go-grow/app
 COPY --from=Build /opt/go-grow/env.env /opt/go-grow/env.env
--- config/database.go
+++ config/database.go
@@ -3,18 +3,18 @@ package config
 import (
 	"fmt"
 	"log"
+	"os"
 
-	"github.com/spf13/viper"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
 
 func ConnectDB() (*gorm.DB, error) {
 	GetEnvConfig()
-	dbUser := viper.Get("DB_USER")
-	dbPassword := viper.Get("DB_PASSWORD")
-	dbHost := viper.Get("DB_HOST")
-	dbName := viper.Get("DB_NAME")
+	dbUser := os.Getenv("DB_USER")
+	dbPassword := os.Getenv("DB_PASSWORD")
+	dbHost := os.Getenv("DB_HOST")
+	dbName := os.Getenv("DB_NAME")
 
 	dsn := fmt.Sprintf("%s:%s@tcp(%s)/%s?tls=true&charset=utf8mb4&parseTime=True&loc=Local", dbUser, dbPassword, dbHost, dbName)
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
