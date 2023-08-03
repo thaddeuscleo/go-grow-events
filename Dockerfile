56b24a723216e5bce3ba6c185cb600a72eb261f6
Thaddeus Cleo <thaddeuscleo@gmail.com>
2023-08-03 07:58:53 +0000

    feat: add container manifest

---
+++ Dockerfile
@@ -0,0 +1,20 @@
+# Build Stage
+FROM golang:1.20.7-alpine AS Build
+
+WORKDIR /opt/go-grow
+COPY . .
+
+RUN go mod download
+RUN go build -o /opt/go-grow/app cmd/app/main.go
+
+
+# Deploy Image
+FROM alpine:3.18
+
+WORKDIR /opt/go-grow
+COPY --from=Build /opt/go-grow/app /opt/go-grow/app
+COPY --from=Build /opt/go-grow/env.env /opt/go-grow/env.env
+
+EXPOSE 8080
+
+# ENTRYPOINT ["/opt/go-grow/app"]
--- config/database.go
+++ config/database.go
@@ -15,9 +15,8 @@ func ConnectDB() (*gorm.DB, error) {
 	dbPassword := viper.Get("DB_PASSWORD")
 	dbHost := viper.Get("DB_HOST")
 	dbName := viper.Get("DB_NAME")
-	dbCharset := viper.Get("DB_CHARSET")
 
-	dsn := fmt.Sprintf("%s:%s@tcp(%s)/%s?charset=%s&parseTime=True&loc=Local", dbUser, dbPassword, dbHost, dbName, dbCharset)
+	dsn := fmt.Sprintf("%s:%s@tcp(%s)/%s?tls=true&charset=utf8mb4&parseTime=True&loc=Local", dbUser, dbPassword, dbHost, dbName)
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
 		log.Fatal(err.Error())
